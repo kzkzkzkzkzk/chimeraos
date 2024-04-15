039090ea5bdabe340be2ce48327e689b095bebf5
kzkzkzkzkzk <121371741+kzkzkzkzkzk@users.noreply.github.com>
2024-04-15 17:39:25 +0900

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -151,10 +151,11 @@ Subsystem	sftp	/usr/lib/ssh/sftp-server
 " > /etc/ssh/sshd_config
 
 echo "
-LABEL=frzr_root /var       btrfs     defaults,subvolid=256,rw,noatime,nodatacow,nofail                                                                                                                                           0   0
-LABEL=frzr_root /home      btrfs     defaults,subvolid=257,rw,noatime,nodatacow,nofail                                                                                                                                           0   0
-LABEL=frzr_root /frzr_root btrfs     defaults,subvolid=5,rw,noatime,nodatacow,x-initrd                                                                                                                                           0   2
-overlay         /etc       overlay   defaults,x-systemd.requires=/frzr_root,x-systemd.rw-only,lowerdir=/sysroot/etc,upperdir=/sysroot/frzr_root/etc,workdir=/sysroot/frzr_root/.etc,index=off,metacopy=off,comment=etcoverlay    0   0
+LABEL=frzr_root /          btrfs subvol=deployments/${SYSTEM_NAME}-${VERSION},ro,noatime,nodatacow 0 0
+LABEL=frzr_root /var       btrfs subvol=var,rw,noatime,nodatacow 0 0
+LABEL=frzr_root /home      btrfs subvol=home,rw,noatime,nodatacow 0 0
+LABEL=frzr_root /frzr_root btrfs subvol=/,rw,noatime,nodatacow 0 0
+LABEL=frzr_efi  /boot      vfat  rw,noatime,nofail  0 0
 " > /etc/fstab
 
 echo "
@@ -207,10 +208,9 @@ rm -rf \
 rm -rf ${FILES_TO_DELETE}
 
 # create necessary directories
-mkdir -p /home
-mkdir -p /var
-mkdir -p /frzr_root
-mkdir -p /efi
+mkdir /home
+mkdir /var
+mkdir /frzr_root
 EOF
 
 #defrag the image
